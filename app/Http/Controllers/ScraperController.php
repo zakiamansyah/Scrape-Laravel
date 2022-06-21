24ddf975e6e64c20043e9a57e1a80b0475b45155
Amansyah Zaki <zakiamansyah2014@gmail.com>
2022-06-21 17:44:40 +0700

    Update Scaraper Controller

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Exception\GuzzleException;
 use Goutte\Client;
 use Illuminate\Http\Request;
 
@@ -21,7 +22,31 @@ class ScraperController extends Controller
         print_r($page);*/
 
         // dd($page->filter('.ratiobox_content')->text());
-        dd($page->filter('.logo-main')->text());
+        // dd($page->filter('.ratiobox_content')->image());
+
+        // $images = $page->filter('.tabs-menu')->each(function ($item) {
+        //     $
+        // });
+
+        $title = $page->filter('.plain')->text();
+        $datepublished = $page->filter('.dateonline')->text();
+        $description = $page->filter('.teaser-link')->text();
+        $link = $page->filter('a')->link();
+
+        if(!empty ($link_r = $link->getUri()))
+        {
+            $image = $page->filter('img')->image();
+            $image_s = $image->getUri();
+            $filename = basename($image_s);
+            $image_path = ('news-gallery/' . $filename);
+        }
+
+        var_dump($image_path);
+        // $coba = $page->filter('.tabs-menu')->each(function ($item){
+        //         $this->result[$item->filter('h2')->text()] = $item->filter('.tabs-menu')->text();
+        // });
+
+        // dd($coba);
 
         // $page->filter('#maincounter-wrap')->each(function ($item) {
         //     $this->results[$item->filter('h1')->text()] = $item->filter('#maincounter-wrap')->text();
