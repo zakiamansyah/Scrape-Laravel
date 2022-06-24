f29d978b1a2b26f32a8bcd5ee366f47320e8a543
Amansyah Zaki <zakiamansyah2014@gmail.com>
2022-06-24 18:09:56 +0700

    fixing scraping with methode crawler

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -3,26 +3,12 @@
 namespace App\Http\Controllers;
 
 use Exception;
-use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\DomCrawler\Crawler;
 
-
-
 class CrawlerController extends Controller
 {
-    // public function crawler()
-    // {
-    //     $get = file_get_contents('https://www.detik.com/tag/virus-corona');
-    //     $crawler = new Crawler($get);
-
-    //     $coba = $crawler->filter('.tabs-menu')->text();
-
-    //     dd($coba);
-    // }
-
     private $client;
 
     public function __construct()
@@ -33,42 +19,45 @@ class CrawlerController extends Controller
         ]);
     }
 
-    private function getStr($start, $end, $string) {
-        if (!empty($string)) {
-        $setring = explode($start,$string);
-        $setring = explode($end,$setring[1]);
-        return $setring[0];
+    private function getStr($start, $end, $string)
+    {
+        if (!empty($string))
+        {
+            $getString = explode($start,$string);
+            $getString = explode($end,$getString[1]);
+
+        return $getString[0];
         }
     }
 
     public function getCrawler()
     {
-        try{
-                $response = $this->client->get("https://burst.shopify.com/photos/search?q=face");
-                $content = $response->getBody()->getContents();
-                $last = $this->getStr('<li class="last">','</li>',$content);
-                $last = $this->getStr('/photos/search?page=','&amp;q=',$last);
-                for ($i=1; $i <= $last; $i++) {
+        $response = $this->client->get("https://burst.shopify.com/photos/search?q=face");
+        $content = $response->getBody()->getContents();
+        $pagination = $this->getStr('<li class="last">', '</li>', $content);
+        $pagination = $this->getStr('/photos/search?page=', '&amp;q=', $pagination);
 
+        $i = 1;
+        while (true) {
+            try{
                 $response = $this->client->get("https://burst.shopify.com/photos/search?page={$i}&q=face");
-                $content = $response->getBody()->getContents();
-                $crawler = new Crawler($content);
+            }
+            catch(Exception $e) {
+                break;
+            }
+            $content = $response->getBody()->getContents();
+            $crawler = new Crawler($content);
 
-                $self = $this;
-                $data = $crawler->filter('div.tile--with-overlay')
-                                ->each(function (Crawler $node, $i) use($self) {
-                                    return $self->getNodeContent($node);
-                                });
+            $self = $this;
+            $data = $crawler->filter('div.tile--with-overlay')
+                            ->each(function (Crawler $node, $i) use($self) {
+                                return $self->getNodeContent($node);
+                            });
 
-                // dd($data);die;
+            $string = implode("\n", $data);
 
-                $string = implode("\n", $data);
-
-                Storage::disk('local')->append('foto.txt', $string);
-                sleep(2);
-                }
-        } catch(Exception $e) {
-            echo $e->getMessage();
+            Storage::disk('local')->append('foto-face.txt', $string);
+            $i++;
         }
     }
 
@@ -79,11 +68,6 @@ class CrawlerController extends Controller
 
     public function getNodeContent($node)
     {
-        // $array = [
-        //     'foto' => $this->hasContent($node->filter('.photo-card img')) != false ? $node->filter('.photo-card img')->attr('src') : ''
-        // ];
-
         return $this->hasContent($node->filter('.ratio-box img')) != false ? $node->filter('.ratio-box img')->attr('src') : '';
-        // return $array;
     }
 }
